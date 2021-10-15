d570fc48349f772a14ab940705aef7e29c1760fc
uttam sharma <suttam446@gmail.com>
2021-10-15 17:41:12 +0530

    show right/wrong after the  end of countdown

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const selectedNumber = document.querySelector(".selectedNumber");
 const dice = document.getElementById("dice");
 const countElement = document.querySelector(".count");
 const timerElement = document.querySelector(".timer");
+var selectedId;
 var diceClicked = false;
 var timer;
+var isNumberBtnClicked = false;
 var title = document.querySelector("h1");
 (function () {
   document.querySelector("h1").classList.add("show");
@@ -34,46 +36,27 @@ document.addEventListener("click", (e) => {
 document.querySelectorAll(".number-btn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const clickedBtn = e.target.closest("[data-btn]");
-
+    isNumberBtnClicked = true;
     clickedBtn.classList.toggle("active");
     setTimeout(() => {
       clickedBtn.classList.remove("active");
     }, 500);
-    clearInterval(timer);
-    count = 5;
 
-    const selectedId = clickedBtn.getAttribute("id");
+    selectedId = clickedBtn.getAttribute("id");
     selectedNumber.innerHTML = selectedId.replace("btn-", "");
-    if (selectedId.replace("btn-", "") == randomNumber) {
-      showMsg("You was right!!");
-      score += randomNumber;
-      document.querySelector(".score").innerHTML = score;
-      randomNumber = getRandomInt(1, 6);
-
-      setTimeout(() => {
-        changeDiceFace(randomNumber);
-        setTimer();
-      }, 1000);
-    } else {
-      showMsg("You was wrong!!");
-
-      randomNumber = getRandomInt(1, 6);
-      setTimeout(() => {
-        changeDiceFace(randomNumber);
-        setTimer();
-      }, 1000);
-    }
   });
 });
 
+//to show messages like 'you was right'
 function showMsg(message) {
   msg.classList.add("show");
   msg.innerHTML = message;
   setTimeout(() => {
     msg.classList.remove("show");
     msg.innerHTML = "";
-  }, 2000);
+  }, 1000);
 }
+// to change dice face provide a number as argument.
 function changeDiceFace(number) {
   let dice = document.getElementById("dice");
   dice.src = `Images/${number}.png`;
@@ -84,21 +67,56 @@ function changeDiceFace(number) {
   }, 500);
 }
 
+// to setTimer which will decrement from 5 to 1.
 function setTimer() {
   timer = setInterval(() => {
     countElement.innerHTML = count;
     count = count - 1;
-    console.log(count);
-    if (count === -1) {
+
+    if (!isNumberBtnClicked && count === -1) {
       const rn = getRandomInt(1, 6);
       clearInterval(timer);
       count = 5;
       changeDiceFace(rn);
       setTimer();
     }
+    if (count == -1 && isNumberBtnClicked) {
+      if (selectedId.replace("btn-", "") == randomNumber) {
+        showMsg("You was right!!");
+        const rn = getRandomInt(1, 6);
+        randomNumber = rn;
+        count = 5;
+
+        score += 1;
+        document.querySelector(".score").innerHTML = score;
+        isNumberBtnClicked = false;
+        clearInterval(timer);
+        setTimeout(() => {
+          selectedNumber.innerHTML = "";
+          changeDiceFace(rn);
+          setTimer();
+        }, 1000);
+      } else {
+        showMsg("You was wrong!!");
+
+        const rn = getRandomInt(1, 6);
+        randomNumber = rn;
+        clearInterval(timer);
+        count = 5;
+        isNumberBtnClicked = false;
+
+        setTimeout(() => {
+          selectedNumber.innerHTML = "";
+
+          changeDiceFace(rn);
+          setTimer();
+        }, 800);
+      }
+    }
   }, 1000);
 }
 
+// this method is used to get random number from 1 to 6.
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
